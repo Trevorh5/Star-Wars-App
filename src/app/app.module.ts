3f146727dc4c4c899d6dc5cb458d621e4a6005b9
trevor <trevor.hallett.5@gmail.com>
2018-05-22 09:11:27 -0600

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { CharacterService } from './services/character.service';
+import { CharacterDetailComponent } from './components/character-detail/character-detail.component';
+import { ForceStrengthComponent } from './components/force-strength/force-strength.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        CharacterListComponent,
+        CharacterDetailComponent,
+        ForceStrengthComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        FormsModule,
+        RouterModule.forRoot([
+          {path: 'characters', component: CharacterListComponent},
+          {path: 'home', component: HomeComponent},
+          {path: '', redirectTo: 'home', pathMatch: 'full'},
+        ])
+    ],
+    providers: [
+        CharacterService
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
